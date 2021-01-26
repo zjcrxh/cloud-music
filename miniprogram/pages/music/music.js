61b2f3fd407e53d1160e8620f0bbf76ebf85cc53
赵嘉纯 <zhaojiachun@zhaojiachundeMacBook-Pro.local>
2021-01-26 21:53:14 +0800

    first commit

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -4,7 +4,70 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
+data: {
+    imgUrls: [{
+      url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'   
+  },
+  {
+      url:'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'
+  },
+  {
+      url:'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
+  },
+  {
+      url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+  },
+  {
+      url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+  },
+  {
+      url:'http://p1.music.126.net/SLfispSeeEnb6Ezs0cNjBw==/109951165666128356.jpg?imageView&quality=89'
+  },
+  {
+      url:'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+  },
+  {
+      url:'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+  }
+],
+playlist:[{
+  
+  "id":"1001",
+  "playCount":"1224000",
+  "name":"[华语速爆新歌] 那英×姚晨首度联手合唱",
+  "picUrl":"http://p1.music.126.net/qfKmb5grEFZNTGYKIONg1A==/109951165664253358.jpg?param=140y140"
+},
+{
+  "id":"1002",
+  "playCount":"2676000",
+  "name":"随情节流淌|富有叙事感的柔软音乐片段",
+  "picUrl":"http://p1.music.126.net/1hzImIzOsRzG0iBSvHOO7w==/109951165511341127.jpg?param=140y140"
+},
+{
+  "id":"1003",
+  "playCount":"356000",
+  "name":"我试着把孤独藏进耳机",
+  "picUrl":"http://p1.music.126.net/Xvo6PwBcdOA69ipcpV9YYg==/109951165463253777.jpg?param=140y140"
+},
+{
+  "id":"1004",
+  "playCount":"6400000",
+  "name":" #幸福# 有幸被爱，余生是你 #云村广播小站#",
+  "picUrl":"http://p1.music.126.net/0u7dhriB29wrDp_GyCrLFw==/109951165664303246.jpg?param=140y140"
+},
+{
+  "id":"1005",
+  "playCount":"7866660",
+  "name":"【翻/原】温柔不是我说 而是你觉得.",
+  "picUrl":"http://p1.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg?param=140y140"
+},
+{
+  "id":"1006",
+  "playCount":"36770000",
+  "name":" 喜欢就是要直接见面",
+  "picUrl":"http://p1.music.126.net/_rFjQsgRJfTQT55Wuv7T2Q==/109951165660082678.jpg?param=140y140"
+}
+]
 
   },
 
